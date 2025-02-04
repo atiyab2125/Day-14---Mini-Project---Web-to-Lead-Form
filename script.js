53d0b09d6ea55c0e24db201e80b6190a13d42a8e
Atyab Ahmad <atyabahmad99@gamil.com>
2025-02-04 09:42:30 +0530

    Recaptcha verification

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
-function beforeSubmit(){
-    let opDate = document.getElementById("op_lead_date");
-    let ipDate = document.getElementById("ip_lead_date"); //Here the input date is a string
-    //So we need to convert it into date fromat as per the locale of the user who is the default lead owner
-    //To check the default lead owner, go to web-to-lead under setup
-    //To check locale, run code, Sytem.debug(userInfo.getLocale());
+let captcha = false;
 
-    let formattedDate = new Date(ipDate.value).toLocaleDateString("en-IN");
-    opDate.value = formattedDate;
+function beforeSubmit(event){
+    if(captcha){
+        let opDate = document.getElementById("op_lead_date");
+        let ipDate = document.getElementById("ip_lead_date"); //Here the input date is a string
+        //So we need to convert it into date fromat as per the locale of the user who is the default lead owner
+        //To check the default lead owner, go to web-to-lead under setup
+        //To check locale, run code, Sytem.debug(userInfo.getLocale());
+
+        let formattedDate = new Date(ipDate.value).toLocaleDateString("en-IN");
+        opDate.value = formattedDate;
+    }else{
+        alert("Please check captcha to proceed");
+        event.preventDefault();
+    }
 }
 
 function timestamp() {
@@ -17,4 +24,9 @@ function timestamp() {
         document.getElementsByName("captcha_settings")[0].value = JSON.stringify(elems);
     }
 }
-setInterval(timestamp, 500)
+setInterval(timestamp, 500);
+
+
+function captchaChecked(){
+    captcha = true;
+}
